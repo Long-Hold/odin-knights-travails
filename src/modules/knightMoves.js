54514ccbfdeeec76e21921b5d07e3bc247e4c47f
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2026-03-05 16:55:20 -0500

    feat: Add pseudocode docstring to loop

--- src/modules/knightMoves.js
+++ src/modules/knightMoves.js
@@ -7,7 +7,33 @@
  * @returns {[[number, number]]} An array of coordinates that must be visited to arrive to the end.
  */
 export function knightMoves(start, end) {
+    const startingSquare = new Node(start);
+    const endSquare = new Node(end);
 
+    /**
+     * Stores the nodes that can be legally visited from the current node,
+     * as a Node is removed from queue, all of it's valid children will be calculated so on
+     * and so forth.
+     */
+    const queue = [startingSquare];
+
+    // Tracks visited spaces so prevent redundant calculations.
+    const visitedSquares = new Set();
+    visitedSquares.add(startingSquare.position.toString());
+    while (queue.length > 0) {
+        const currentNode = queue.shift();
+        /**
+         * At most, 8 squares can be visited from a single starting point.
+         * 
+         * (x - 2 & x + 2, y + 1 & y - 1)
+         * (y + 2 & y - 2, x + 1 & x - 1)
+         * 
+         * As a node is visited and added to the queue, also add it to the set.
+         * 
+         * If any are out-of-bounds or in visitedSquares, ignore, else add to queue.
+         * If coord = end.position, terminate the loop.
+         */
+    }
 }
 
 /**
