b40b63e5c0e804dd1b9d3b47c1a2185de57814ec
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2026-03-05 15:15:23 -0500

    feat: Add initial docstring to function

---
+++ src/modules/knightMoves.js
@@ -0,0 +1,11 @@
+/**
+ * Constructs an array of coordinates that show the shortest path a Knight piece
+ * can take to reach the end coordinates, starting from the start coordinates.
+ * 
+ * @param {[number, number]} start - The starting coordinates for the Knight.
+ * @param {[number, number]} end - The end coordinates the Knight must traverse to.
+ * @returns {[[number, number]]} An array of coordinates that must be visited to arrive to the end.
+ */
+export function knightMoves(start, end) {
+
+}
