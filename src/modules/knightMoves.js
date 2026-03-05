8e7de6cda46e44102d02a6713095482e53735bc6
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2026-03-05 15:54:30 -0500

    feat: Create Node class to represent chess squares

--- src/modules/knightMoves.js
+++ src/modules/knightMoves.js
@@ -9,3 +9,21 @@
 export function knightMoves(start, end) {
 
 }
+
+/**
+ * A node that represents a square on a chess board.
+ * 
+ * Nodes are connected to their parent node as they are discovered as a valid, traversable
+ * square from their parent node.
+ */
+class Node {
+    /**
+     * 
+     * @param {[number, number]} position - The position of the Node on a chess board.
+     * @param {Node} parent - The Node that was visited prior to discovering this node.
+     */
+    constructor(position, parent = null) {
+        this.position = position;
+        this.parent = parent;
+    }
+}
